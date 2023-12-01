056e921cd5808131199c0e45e15f022bb4069edc
Attila Balint <attila.balint@kuleuven.be>
2023-12-01 10:30:28 +0100

    first sktime (NaiveForecaster) model

---
+++ models/st-naive/src/main.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from sktime.forecasting.naive import NaiveForecaster
+
+from enfobench import AuthorInfo, ForecasterType, ModelInfo
+from enfobench.evaluation.server import server_factory
+from enfobench.evaluation.utils import create_forecast_index
+
+
+class NaiveForecasterModel:
+    def info(self) -> ModelInfo:
+        return ModelInfo(
+            name="Sktime.NaiveForecaster.Mean",
+            authors=[AuthorInfo(name="Attila Balint", email="attila.balint@kuleuven.be")],
+            type=ForecasterType.point,
+            params={
+                "strategy": "mean",
+            },
+        )
+
+    def forecast(
+        self,
+        horizon: int,
+        history: pd.DataFrame,
+        past_covariates: pd.DataFrame | None = None,
+        future_covariates: pd.DataFrame | None = None,
+        level: list[int] | None = None,
+        **kwargs,
+    ) -> pd.DataFrame:
+        # Fill missing values
+        y = history.y.fillna(history.y.mean())
+
+        # Create model
+        model = NaiveForecaster(strategy="mean")
+
+        # Make forecast
+        index = create_forecast_index(history=history, horizon=horizon)
+        pred: pd.Series = model.fit_predict(y, fh=index, **kwargs)
+
+        # Postprocess forecast
+        forecast = pred.to_frame("yhat").fillna(y.mean())
+        return forecast
+
+
+# Instantiate your model
+model = NaiveForecasterModel()
+
+# Create a forecast server by passing in your model
+app = server_factory(model)
